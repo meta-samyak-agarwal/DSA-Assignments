7eb536071c2cc44a4777c39a4144798c83504387
meta-samyak-agarwal <samyak.agarwal@metacube.com>
2025-04-03 18:46:17 +0530

    Update Assignment1.java

--- Stack_Queues (class 2)/Assignment1.java
+++ Stack_Queues (class 2)/Assignment1.java
@@ -1,111 +1,164 @@
-
-class Stack<T> { // Using generics (T represents the type)
-    private int maxSize; // Maximum size of the stack
-    private T[] stackArray; // Array to store elements of the stack
-    private int top; // Index of the top element
-    
-    @SuppressWarnings("unchecked")
-    public Stack(int size) {
-        maxSize = size;
-        stackArray = (T[]) new Object[size];
-        top = -1; // Stack is initially empty
-    }
-
-    public boolean isEmpty() {
-        return (top == -1);
-    }
-
-    public boolean isFull() {
-        return (top == maxSize - 1);
-    }
-
-    public void push(T element) {
-        if (!isFull()) {
-            stackArray[++top] = element;
-        } else {
-            throw new RuntimeException("Stack overflow");
-        }
-    }
-
-    public T pop() {
-        if (!isEmpty()) {
-            return stackArray[top--];
-        } else {
-            throw new RuntimeException("Stack underflow");
-        }
-    }
-
-    public T peek() {
-        if (!isEmpty()) {
-            return stackArray[top];
-        } else {
-            throw new RuntimeException("Stack is empty");
-        }
-    }
-}
+import java.util.Arrays;
+import java.util.EmptyStackException;
+import java.util.List;
 
 class Assignment1 {
 
-    
-// Simplified method to determine precedence of operators
-    private static int precedence(String operator) {
-        return switch (operator) {
-            case "+", "-" -> 1; // Lowest precedence among these operators
-            case "*", "/" -> 2; // Higher precedence than + and -
-            case "(" -> 0;      // Parentheses do not have precedence in evaluation
-            default -> -1;      // Invalid or unrecognized operator
-        };
+    // Define Stack interface
+    interface Stack<T> {
+        void push(T x);
+
+        T pop();
+
+        T top();
+
+        int size();
+
+        boolean isEmpty();
     }
 
-    // Method to convert infix expression to postfix
-    private static String infixToPostfix(String infixExpression) {
+    public static class ArrayStack<T> implements Stack<T> {
+
+        private int top;
+        private T[] arr;
+        private int maxSize;
+
+        // Constructor
+        public ArrayStack(int maxSize) {
+            this.maxSize = maxSize;
+            this.arr = (T[]) new Object[maxSize];
+            this.top = -1;
+        }
+
+        @Override
+        public void push(T x) {
+            if (top + 1 == maxSize)
+                throw new StackOverflowError("Stack is full");
+            arr[++top] = x;
+        }
+
+        @Override
+        public T pop() {
+            if (isEmpty())
+                throw new EmptyStackException();
+            return arr[top--];
+        }
+
+        @Override
+        public T top() {
+            if (isEmpty())
+                throw new EmptyStackException();
+            return arr[top];
+        }
+
+        @Override
+        public int size() {
+            return top + 1;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return top == -1;
+        }
+    }
+
+    static int precedence(String s) {
+        switch (s) {
+            case "/", "*":
+                return 7;
+            case "+", "-":
+                return 6;
+            case ">", "<", ">=", "<=":
+                return 5;
+            case "==", "!=":
+                return 4;
+            case "!":
+                return 3;
+            case "&&":
+                return 2;
+            case "||":
+                return 1;
+            default:
+                return -1;
+        }
+    }
+
+    static String infixToPostfix(String infix, ArrayStack<String> st) {
+        List<String> tokens = Arrays.asList(infix.split(" "));
         StringBuilder postfix = new StringBuilder();
-        Stack<String> stack = new Stack<>(infixExpression.length());
-        String[] tokens = infixExpression.split(" ");
 
         for (String token : tokens) {
-            if (token.matches("\\d+")) { // Token is an integer
+            if (token.matches("\\d+")) {
                 postfix.append(token).append(" ");
-            } else if (token.equals("(")) { // Left parenthesis
-                stack.push(token);
-            } else if (token.equals(")")) { // Right parenthesis
-                while (!stack.isEmpty() && !stack.peek().equals("(")) {
-                    postfix.append(stack.pop()).append(" ");
+            } else if ("(".equals(token)) {
+                st.push(token);
+            } else if (")".equals(token)) {
+                while (!st.isEmpty() && !"(".equals(st.top())) {
+                    postfix.append(st.pop()).append(" ");
                 }
-                stack.pop(); // Pop '('
-            } else { // Operator
-                while (!stack.isEmpty() && precedence(stack.peek()) >= precedence(token)) {
-                    postfix.append(stack.pop()).append(" ");
+                st.pop(); // Remove '('
+            } else {
+                while (!st.isEmpty() && precedence(st.top()) >= precedence(token)) {
+                    postfix.append(st.pop()).append(" ");
                 }
-                stack.push(token);
+                st.push(token);
             }
         }
 
-        // Pop remaining operators in the stack
-        while (!stack.isEmpty()) {
-            postfix.append(stack.pop()).append(" ");
+        while (!st.isEmpty()) {
+            postfix.append(st.pop()).append(" ");
         }
 
         return postfix.toString().trim();
     }
 
-    // Method to evaluate postfix expression
-    private static int evaluatePostfix(String postfixExpression) {
-        Stack<Integer> stack = new Stack<>(postfixExpression.length());
-        String[] tokens = postfixExpression.split(" ");
+    static int evaluatePostfix(String postfix) {
+        ArrayStack<Integer> stack = new ArrayStack<>(1000);
+        List<String> tokens = Arrays.asList(postfix.split(" "));
 
         for (String token : tokens) {
-            if (token.matches("\\d+")) { // Operand
+            if (token.matches("\\d+")) {
                 stack.push(Integer.parseInt(token));
-            } else { // Operator
-                int operand2 = stack.pop();
-                int operand1 = stack.pop();
+            } else {
+                int b = stack.pop(), a = stack.pop();
+                
                 switch (token) {
-                    case "+" -> stack.push(operand1 + operand2);
-                    case "-" -> stack.push(operand1 - operand2);
-                    case "*" -> stack.push(operand1 * operand2);
-                    case "/" -> stack.push(operand1 / operand2);
-                    case "%" -> stack.push(operand1 % operand2);
+                    case "+":
+                        stack.push(a + b);
+                        break;
+                    case "-":
+                        stack.push(a - b);
+                        break;
+                    case "*":
+                        stack.push(a * b);
+                        break;
+                    case "/":
+                        stack.push(a / b);
+                        break;
+                    case ">":
+                        stack.push(a > b ? 1 : 0);
+                        break;
+                    case "<":
+                        stack.push(a < b ? 1 : 0);
+                        break;
+                    case ">=":
+                        stack.push(a >= b ? 1 : 0);
+                        break;
+                    case "<=":
+                        stack.push(a <= b ? 1 : 0);
+                        break;
+                    case "==":
+                        stack.push(a == b ? 1 : 0);
+                        break;
+                    case "!=":
+                        stack.push(a != b ? 1 : 0);
+                        break;
+                    case "&&":
+                        stack.push((a != 0 && b != 0) ? 1 : 0);
+                        break;
+                    case "||":
+                        stack.push((a != 0 || b != 0) ? 1 : 0);
+                        break;
                 }
             }
         }
@@ -114,9 +167,11 @@ class Assignment1 {
     }
 
     public static void main(String[] args) {
-        String infixExpression = "3 + 5 * ( 2 - 8 )";
-        String postfixExpression = infixToPostfix(infixExpression);
-        System.out.println("Postfix: " + postfixExpression);
+        ArrayStack<String> st = new ArrayStack<>(1000);
+        String infix = "2 + 3 * ( 5 - 2 ) >= 4";
+
+        String postfixExpression = infixToPostfix(infix, st);
+        System.out.println("Postfix Expression: " + postfixExpression);
 
         int result = evaluatePostfix(postfixExpression);
         System.out.println("Result: " + result);
